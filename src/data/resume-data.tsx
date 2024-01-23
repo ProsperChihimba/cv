335fda51b23b82c529b11b45f1ed7d6b6e4154d0
Prosper Absalom <prosperchihimba@gmail.com>
2024-01-23 20:59:16 +0300

    Updated cv details -> Prosper absalom

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,278 +1,153 @@
 import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
   ClevertechLogo,
   ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
   MonitoLogo,
-  NSNLogo,
   ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
 } from "@/images/logos";
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Prosper Absalom",
+  initials: "PA",
+  location: "Dar Es Salaam, Tanzania",
+  locationLink: "https://www.google.com/maps/place/Dar+es+Salaam",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "Full Stack Engineer with a focus on building financial products",
   summary:
     "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+  avatarUrl: "https://avatars.githubusercontent.com/u/72831371?s=96&v=4",
+  personalWebsiteUrl: "https://prosperabsalom.com",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "howdy@prosperabsalom.com",
+    tel: "+255747696485",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/ProsperChihimba",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/prosper-absalom/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://x.com/Prosper_absalom",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "University of Dar es Salaam",
+      degree: "Bachelor's Degree in Business Infromation Technology",
+      start: "2023",
+      end: "2026",
+    },
+    {
+      school: "Sokoine University of agriculture",
+      degree: "Diploma In Information Technology",
+      start: "2020",
+      end: "2022",
     },
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
+      company: "Swahilies Inc ",
+      link: "https://swahilies.com",
       badges: ["Remote"],
-      title: "Senior Full Stack Developer",
+      title: "Software Engineer ",
       logo: ParabolLogo,
-      start: "2021",
+      start: "2020",
+      end: "Current",
+      description:
+        "Built a banking feature for SMEs to access all banking services, Rebuilt mobile app for SMEs bookkeeping and banking, Built a web version of the mobile app for SMEs to access bookkeeping through desktops, Built main internal tools(Dashboards) for management and the company main website. Technologies: React, TypeScript, Flutter, Python",
+    },
+    {
+      company: "Cheka Plus",
+      link: "https://chekaplus.com",
+      badges: ["Remote"],
+      title: "System administrator → Full Stack Developer",
+      logo: ClevertechLogo,
+      start: "2023",
       end: "2024",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
-    },
-    {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
-      description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
-    },
-    {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
-      description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+        "Led the development of a mini-app version of Cheka Plus, strategically aligning with Vodacom's M-Pesa app, System Maintenance and Updates of the Cheka Plus mobile application and server infrastructure. Technologies: Flutter, Python, React, TypeScript, JavaScript, Ubuntu, AWS",
     },
   ],
   skills: [
+    "Python",
+    "Flutter",
+    "Dart",
     "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "React",
+    "Django",
+    "SQL",
+    "Linux",
+    "AWS",
+    "Git"
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "Momoa",
       techStack: [
-        "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "Flutter",
+        "Python",
+        "Django",
+        "React",
+        "AWS",
       ],
-      description: "A platform to build and grow your online business",
+      description: "Mobile application to help agents to keep track of their cashflow, debt customers, expenses and commissions",
       logo: ConsultlyLogo,
       link: {
-        label: "consultly.com",
-        href: "https://consultly.com/",
+        label: "momoa.com",
+        href: "https://drimer.io/@downloadmomoa",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "Shoket",
+      techStack: ["Side Project", "Python", "Django", "Payment API"],
       description:
-        "Browser extension that records everything happening in a web application",
+        "Stripe for developers, a payment processor for developers and small business to easly access payment.",
       logo: MonitoLogo,
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
+        label: "shoket.co",
+        href: "https://shoket.co/",
       },
     },
     {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
+      title: "MedPack",
+      techStack: ["Client Project", "React", "TypeScript", "JavaScript"],
       description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
+        "Built website and dashboard for company dealing with Health Facilities products for managing their procurements, inventory and sales.",
+      logo: MonitoLogo,
       link: {
-        label: "github.com",
-        href: "https://jarocki.me/",
+        label: "medpack.co.tz",
+        href: "https://medpack.co.tz/",
       },
     },
     {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "Flutter Tanzania",
+      techStack: ["Flutter", "Dart", "Writing", "Community Development"],
       description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
+        "A community for Flutter developers to learn and share knowledge",
+      logo: MonitoLogo,
       link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
+        label: "dev.to/fluttertanzania",
+        href: "https://dev.to/fluttertanzania",
       },
     },
     {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "Kipenzi",
+      techStack: ["Client Project", "Flutter", "Dart", "Python"],
       description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
+        "Tinder for Tanzania, building a dating mobile application for Tanzanians",
+      logo: MonitoLogo,
       link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
+        label: "#",
+        href: "#",
       },
-    },
-    {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
-      link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
-      },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
